882b79d2047fa6963c7261e6bb7a9d312b9535d4
ManilkinViktor <manilkin99@bk.ru>
2026-01-30 17:49:58 +0500

    refactor validation in schemas/mixins.py

--- src/schemas/mixins.py
+++ src/schemas/mixins.py
@@ -1,8 +1,9 @@
 import datetime, uuid
+from typing import ClassVar
 
-from pydantic import BaseModel, field_validator, validator
+from pydantic import BaseModel, field_validator
 
-from utils.datetime_utils import utc_now
+from utils.datetime_utils import validate_utc_timezone, validate_not_future_time
 
 class TimeStampMixinDTO(BaseModel):
     created_at: datetime.datetime
@@ -11,40 +12,26 @@ class TimeStampMixinDTO(BaseModel):
     @field_validator('created_at', 'updated_at')
     @classmethod
     def validate_datetime(cls, v: datetime.datetime) -> datetime.datetime:
-        v = cls.validate_utc_timezone(v)
-        v = cls.validate_not_future_time(v)
+        v = validate_utc_timezone(v)
+        v = validate_not_future_time(v)
         return v
 
-
-    @classmethod
-    def validate_utc_timezone(cls, v:datetime.datetime) -> datetime.datetime:
-        if v.tzinfo is None:
-            raise ValueError('datetime must have timezone')
-        if v.tzinfo != datetime.UTC:
-            v = v.astimezone(datetime.UTC)
-
-        return  v
-
-
-    @classmethod
-    def validate_not_future_time(cls, v: datetime.datetime) -> datetime.datetime:
-        if v > utc_now():
-            raise ValueError('datetime cannot be future')
-        return v
 
 
 
 
 class UUIDMixinDTO(BaseModel):
     id: uuid.UUID
-    _uuid_version = 7
+    _uuid_version: ClassVar[int] = 7
 
     @field_validator('id')
     @classmethod
-    def validate_uuid_version(cls, v: uuid.UUID) -> uuid.UUID:
+    def validate_uuid_version(cls, v: uuid.UUID | str) -> uuid.UUID:
+        if isinstance(v, str):
+            v = uuid.UUID(v)
         if  v.version != cls._uuid_version:
             raise ValueError(
-                f'incorrect uuid version. \
-                allowed: {cls._uuid_version}'
+                f'incorrect uuid version. allowed: {cls._uuid_version}'
             )
         return v
+
